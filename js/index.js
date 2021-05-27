94291ef39c3c63e55322ea33847ecbf80c0383c3
Devsabirul <84068748+Devsabirul@users.noreply.github.com>
2021-05-27 21:43:31 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function GeneratePic(){
+    const generateNum = '0123456789';
+        var ShowPin = "";
+    for (let i = 0; i <4; i++) {
+        var generatePin = Math.floor(Math.random() * generateNum.length);
+        ShowPin += generateNum.substring(generatePin,generatePin+1); 
+    }
+    document.getElementById("pin_output").value = ShowPin; 
+};
+
+function removeInput(id){
+    document.getElementById(id).value="";
+}
+
+
+//Generate pin With Submit Number For Matched
+
+document.getElementById('submit_btn').addEventListener('click',function(){
+    var generatePinNum = document.getElementById("pin_output").value;
+    var submitPinNum = document.getElementById('showNum').value;
+
+     removeInput('pin_output');
+     removeInput('showNum');
+
+    if(generatePinNum == submitPinNum){
+        document.getElementById('demo').innerHTML='✅ Pin Matched... Secret door is opening for you';
+    }else{
+        document.getElementById('demo').innerHTML = "❌ Pin Didn't Match, Please try again";
+        document.getElementById('demo1').innerHTML = "3 try left";
+    }
+})
